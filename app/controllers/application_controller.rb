1eab1d3b98bca4b4da0c8282e63081b1949cc28d
aaron13michael <ams8560@rit.edu>
2016-03-04 08:51:36 +0000

    debug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
+require 'rubygems'
+gem 'yahoo-weather'
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
