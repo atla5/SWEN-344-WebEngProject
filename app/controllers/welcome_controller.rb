84de7ddd3d332ab8a39f94898c9a6b352372306f
AcidHotpocket <rej7869@rit.edu>
2016-03-05 10:29:10 -0500

    Removed local testing weather widget from sign in page.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,6 @@
 class WelcomeController < ApplicationController
-  
-  require 'open_weather'
-
   # GET /welcome
   def index
-    @current = OpenWeather::Current.city_id("5134086", { units: "imperial", APPID: "106fc5306b995d8409aa88eb9cc548d4" })
   end
 
 end
