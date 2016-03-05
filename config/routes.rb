14d471f74a2056283e2b6b9317aeac07a94d6549
Aidan Sawyer <aks5238@rit.edu>
2016-03-05 20:45:52 +0000

    remove root_path in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,6 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
-  root_path = root
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
