31da6f192d1b20114a7f2bd0e91a56e53a42c822
aaron13michael <ams8560@rit.edu>
2016-03-05 21:07:50 +0000

    removed duplicate routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,9 +15,6 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/auth/twitter/callback', to: 'sessions#create', as: 'callback'
   get '/auth/failure', to: 'sessions#error', as: 'failure'
-
-  get '/profile', to: 'sessions#show', as: 'show'
-  delete '/signout', to: 'sessions#destroy', as: 'signout'
   post '/writetweet', to: 'sessions#writetweet', as: 'write_tweet'
   resources :widgets
 
