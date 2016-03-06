5be36584dc3ed6843ab66a0647c9f18c4c57e191
eyoon13 <eay5268@rit.edu>
2016-03-06 03:41:32 +0000

    added and updated the top 5 stocks on the main page.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
       @user = client.user(include_entities: true)
       @tweets = client.home_timeline[0..10]
       @current = OpenWeather::Current.city_id("5134086", { units: "imperial", APPID: "106fc5306b995d8409aa88eb9cc548d4" })
-      @stocks = stocklist
+      @stocks = getStocks
     else
       redirect_to failure_path
     end
@@ -35,11 +35,10 @@ class SessionsController < ApplicationController
     yahoo_client = YahooFinance::Client.new
     data = yahoo_client.quotes(['AAPL','MSFT','JPC', 'TWTR', 'LUV' ], [:name, :ask, :bid, :last_trade_date])
     cuddle =[]
-    data.each { |x| slut = x.name + " has a value: " + x.ask
+    data.each { |x| slut = x.name + "|value: " + x.ask + "| end:" + x.bid + '|'
         cuddle << slut
     }
-        
-    @stocks = cuddle
+    return cuddle
   end
 
   def error
@@ -50,15 +49,6 @@ class SessionsController < ApplicationController
   def destroy
     reset_session
     redirect_to root_path, notice: 'Signed out'
-  end
-  
-   #function to pull 5 stocks with their [ ]
-  def stocklist
-    stocks = ['AAPL', 'MSFT', 'BAC', 'JCP', 'JPM']
-    columns = [:ask, :bid, :last_trade_date]
-    ycl = YahooFinance::Client.new
-    mainList = ycl.quote(stocks,columns)
-    return mainList
   end
   
 end
