4740fce0f9b2a3bcd526f9553daf1c08e4f5efa2
Aidan Sawyer <aks5238@rit.edu>
2016-03-06 03:34:32 +0000

    add initial, limited stocks page

--- app/controllers/stock_controller.rb
+++ app/controllers/stock_controller.rb
@@ -5,7 +5,10 @@ class StockController < ApplicationController
     yahoo_client = YahooFinance::Client.new
     
     def stockList
-         
+        if ! (session['access_token'] && session['access_token_secret'])
+            redirect_to failure_path
+        end
+        
     end
     
 end
