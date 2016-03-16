d9509d68ab8b67d9dc701581fd2c272e9bac6d39
aaron13michael <ams8560@rit.edu>
2016-03-16 15:12:14 +0000

    updated authentication url to use environment variable

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
   # GET /welcome
   def index
+     @authUrl = ENV['AUTH_URL']
      if session['access_token'] && session['access_token_secret']
        redirect_to show_path
      end
