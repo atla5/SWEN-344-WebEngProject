d58164facca4638de09767007ece4996be6bfa6f
aaron13michael <ams8560@rit.edu>
2016-05-05 16:20:29 +0000

    Events created in calendar are saved in databased

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -9,6 +9,14 @@ class CalendarController < ApplicationController
     end
     
     def new_event
+        currUser = User.where(handle: "dan_tester344").take
+        eDate = DateTime.strptime(params[:eventdate], '%Y-%m-%d')
+        e = Event.create(user: currUser,
+                        start_time: eDate,
+                        name: params[:name],
+                        location: params[:location])
+        e.save
+        redirect_to calendar_path
     end
 
 end
