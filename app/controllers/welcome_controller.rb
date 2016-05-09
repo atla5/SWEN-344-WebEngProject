f52502839fa1c88d0b22b9f7f13e616167ed7361
aaron13michael <ams8560@rit.edu>
2016-05-09 13:51:13 +0000

    moved navbar to partial view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,9 @@ class WelcomeController < ApplicationController
   # GET /welcome
   def index
      @authUrl = ENV['AUTH_URL']
+     @loggedIn = false
      if session['access_token'] && session['access_token_secret']
+       @loggedIn = true
        redirect_to show_path
      end
   end
