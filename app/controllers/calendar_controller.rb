4371a3118a4c74a56d5b9ddddfb3fc79d78eb477
eyoon13 <eay5268@rit.edu>
2016-05-09 05:29:46 +0000

    fixed dan_tester

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -4,14 +4,14 @@ class CalendarController < ApplicationController
         #if ! (session['access_token'] && session['access_token_secret'])
             #redirect_to failure_path
         #end
-        @user = User.where(handle: "dan_tester344").take
+        @user = User.where(handle: client.user.screen_name).take
         @events = @user.events
         
     end 
     
 
     def new_event
-        currUser = User.where(handle: "dan_tester344").take
+        currUser = User.where(handle: client.user.screen_name).take
         eDate = DateTime.strptime(params[:eventdate], '%Y-%m-%d')
         e = Event.create(user: currUser,
                         start_time: eDate,
