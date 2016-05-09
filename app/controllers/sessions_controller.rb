888c8c52fe2db2f99fa5fa1471c5e0b655b8b4aa
Aidan Sawyer <aks5238@rit.edu>
2016-05-09 06:26:27 +0000

    fix country/state visibility, lower city display font size

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -47,7 +47,7 @@ class SessionsController < ApplicationController
 
       @current = OpenWeather::Current.geocode(lat, long, { units: "imperial", APPID: "106fc5306b995d8409aa88eb9cc548d4" })
       yahoo_client = YahooFinance::Client.new
-      ycl = yahoo_client.quotes(['AAPL','MSFT','JPC', 'TWTR', 'LUV' ], [:name, :ask, :bid, :high, :low, :change, :symbol, :last_trade_date])
+      ycl = yahoo_client.quotes(['AAPL','MSFT','JPC', 'TWTR', 'LUV' ], [:name, :country, :ask, :bid, :high, :low, :change, :symbol, :last_trade_date])
       @stocks = ycl
     else
       redirect_to failure_path
