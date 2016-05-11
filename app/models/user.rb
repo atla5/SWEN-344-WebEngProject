6768f6c7a9c34bc7f3651e0d017f12ea4b2b67a8
Aidan Sawyer <aks5238@rit.edu>
2016-05-11 10:33:47 +0000

    add some basic helpers/getters to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,34 @@ class User < ActiveRecord::Base
     
     #stocks
     has_many :transactions
+    has_many :stocks_owned
     has_many :stocks_following
     has_one  :balance
+    
+    
+    #add the string for a stockId to private list
+    def addFollowing(stockId)
+        :stocks_following.push(stockId)
+    end
+    
+    # return the number of shares owned of given stockId
+    def num_shares_of_stock(stockId)
+        for stock in :stocks_owned
+            if(stock.id == stockId)
+                return stock.shares;
+            end
+        end
+        return 0;
+    end
+    
+    #return a list of transactions relevant to the stockId
+    def get_transactions(stockId)
+        lsToReturn = []
+        for t in :transactions
+            if(t.stockId == stockId)
+                lsToReturn.push(t)
+            end
+        end
+        return lsToReturn;
+    end
 end
