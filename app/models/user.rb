71eb9cb0cd49683afcd3b9835195d9d9b1d2b853
Aidan Sawyer <aks5238@rit.edu>
2016-05-11 10:34:31 +0000

    add transaction method helpers from  branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,4 +43,58 @@ class User < ActiveRecord::Base
         end
         return lsToReturn;
     end
+    
+    ## - Transaction methods - ##
+    
+    def buy_transaction(t)
+        total_cost = t.getTransactionAmount;
+        
+        if( total_cost > balance )
+            return false;
+        else
+            
+            #add shares of stock to the inventory
+            addToInventory(t.stockId, t.shares);
+            
+            #subtract total amount from balance
+            subtract_from_balance(total_cost);
+            
+            return true;
+            
+        end
+    end
+    
+    
+    #
+    def sell_transaction(t)
+        
+        #check inventory for the right number of shares of given stock
+        
+        #remove stocks from the inventory
+        
+        #add total transaction amount to balance
+        this.balance += t.getTransactionAmount;
+        return true;
+    
+    end
+    
+    
+    ## - helpers - ##
+    
+    # safely subtract from the balance and ensure that there will be no negatives
+    def subtract_from_balance(amt)
+        if(amt >= this.balance)
+            this.balance = 0;
+            return false;
+        else
+            this.balance -= amt;
+            return true;
+        end
+    end
+    
+    # add a given number of shares of a given stock to users inventory
+    def addToInventory(stockId, numShares)
+       ##TODO## 
+    end
+    
 end
